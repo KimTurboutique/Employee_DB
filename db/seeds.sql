c1f7687cff1ff8fe0311b518b34b07389b2c8853
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-06-29 22:23:06 -0400

    added values, prompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,8 +3,9 @@ USE company_db;
 INSERT INTO department(name)
 VALUES ('HR'),('Finance'),('IT'),('Legal');
 
-INSERT INTO role(name)
-VALUES ('Legal Team Lead'), ('Lawyer'), ('Accountant'), ('Customer Service'), ('Sales Lead'), ('Salesperson'), ('Lead Engineer');
+INSERT INTO role(title, salary, department_id)
+VALUES ('Legal Team Lead', 120000, 4), 
+    ('Lawyer', 275000, ), ('Accountant'), ('Customer Service'), ('Sales Lead'), ('Salesperson'), ('Lead Engineer');
 
 
 INSERT INTO employee(name)
--- index.js
+++ index.js
@@ -10,33 +10,32 @@ function menu(){
                 {
                     name:'View all departments',
                     value:'VIEW_DEPARTMENTS',
-                }
-            ]
-        },
-        {
-            type:'list',
-            name:'choice',
-            message:'Select an option',
-            choices:[
+                },
                 {
                     name:'View all roles',
                     value:'VIEW_ROLES'
-                }
-            ]
-        },
-        {
-            type:'list',
-            name:'choice',
-            message:'Select and option',
-            choices:[
+                },
                 {
                     name:'View all employees',
                     value:'VIEW_EMPLOYEES'
-                }
+                },
+                {
+                    name:'Add an Employee',
+                    value:'ADD_EMPLOYEE'
+                },
             ]
-        }
+        }   
     ]).then(response => {
         console.log(response)
+        switch(response.choice) {
+          case "VIEW_EMPLOYEES":
+            viewEmployees();
+            break;
+          case "ADD_EMPLOYEE":
+            // prompt user for employee info then use db.query() mysql2 method to insert employee info into database
+            addEmployee();
+            break;
+        }
     })
     
 }
